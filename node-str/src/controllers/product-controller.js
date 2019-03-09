f1268071c276b8029b094c219c5d43e0a46c28b3
igormatheusdiniz <igor.pinheiro@ccc.ufcg.edu.br>
2019-03-09 09:31:04 -0300

    getById and updateProduct

--- node-str/src/controllers/product-controller.js
+++ node-str/src/controllers/product-controller.js
@@ -26,6 +26,31 @@ exports.getBySlug = (req, res, next) => {
     });
 };
 
+exports.getById = (req, res, next) => {
+    Product.findById(req.params.id)
+    .then (data => {
+        res.status(200).send(data);
+    })
+    .catch(e => {
+        res.status(400).send({message: 'Produto não encontrado', data: e});
+    })
+
+
+}
+
+exports.getByTag = (req, res, next) => {
+    Product
+    .find({
+        tags: req.params.tag
+    }, 'title description price slug tags')
+    .then(data => {
+        res.status(200).send(data);
+    })
+    .catch(e => {
+        res.status(400).send(e);
+    });
+};
+
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
     product.save()
@@ -38,10 +63,20 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({
-        id: id,
-        item: req.body
+    Product
+    .findByIdAndUpdate(
+        req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price
+            }
+    })
+    .then(data => {
+        res.status(200).send({message: 'Produto atualizado com sucesso'});
+    })
+    .catch(e => {
+        res.status(400).send({message: 'Falha ao atualizar o produto', data: e});
     });
 };
 
--- node-str/src/routes/products.js
+++ node-str/src/routes/products.js
@@ -8,6 +8,10 @@ router.get('/', controller.get);
 
 router.get('/:slug', controller.getBySlug);
 
+router.get('/admin/:id', controller.getById);
+
+router.get('/tags/:tag', controller.getByTag);
+
 router.post('/', controller.post);
 
 router.put('/:id', controller.put);
